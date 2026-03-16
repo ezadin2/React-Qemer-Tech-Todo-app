4f0abd2e05d90624844132966237014aba6ca079
Ezadin Badiru <149968485+ezadin2@users.noreply.github.com>
2026-03-16 06:52:59 -0700

    Update import paths for images in TodoItem.jsx

--- src/component/TodoItem.jsx
+++ src/component/TodoItem.jsx
@@ -1,7 +1,6 @@
-// TodoItem.jsx is in src/component/
-import cross from '../assets/cross.png';
-import tick from '../assets/tick.png';
-import not_tick from '../assets/not_tick.png';
+import cross from './assets/cross.png';
+import tick from './assets/tick.png';
+import not_tick from './assets/not_tick.png';
 import './Css/TodoItem.css';
 const TodoItem = ({ no, text, display, setTodos }) => {
   const deleteTodo = (no) => {
